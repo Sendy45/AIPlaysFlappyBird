8074557c943f763e8b75162162e6ea73a7b65106
Sendy45 <itamarsan45@gmail.com>
2025-11-15 22:16:07 +0200

    final commit - best genome weights and full comments

--- Pipe.py
+++ Pipe.py
@@ -3,11 +3,13 @@ import random
 import pygame
 
 class Pipe:
-    opening = 200
+    opening = 200 # gap length
     width = 80
-    velocity = 200
+    velocity = 200 # on x axis
     def __init__(self, x: int, scree_height: int):
+        # Generate opening in random place
         y_opening = random.randint(40, 300)
+        # Pipe made out of 2 rects
         self._top_part = pygame.Rect(x, 0, Pipe.width, y_opening)
         self._bottom_part = pygame.Rect(x, y_opening + Pipe.opening, Pipe.width, scree_height - y_opening - Pipe.opening)
 
